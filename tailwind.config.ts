eb39a7d1e4c4170ad9c36602295d74dbaa620660
Allen Schmerler <allenschmerler@gmail.com>
2024-04-21 01:50:07 -0400

    Implemented a theme swap

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "coffee"],
+    themes: ["light", "dark", "coffee", "retro", "cyberpunk", "valentine", "lofi", "lemonade"],
     base: true,
   },
 };
